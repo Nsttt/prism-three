a0794979034dbf9a4826014ea54be9adf8687631
Nestor <elnestinatorHD@gmail.com>
2022-10-22 18:25:29 +0200

    Add lerp utils

---
+++ src/utils/lerp.ts
@@ -0,0 +1,15 @@
+import * as THREE from "three";
+
+export function lerp(object, prop, goal, speed = 0.1) {
+  object[prop] = THREE.MathUtils.lerp(object[prop], goal, speed);
+}
+
+const color = new THREE.Color();
+export function lerpC(value, goal, speed = 0.1) {
+  value.lerp(color.set(goal), speed);
+}
+
+const vector = new THREE.Vector3();
+export function lerpV3(value, goal, speed = 0.1) {
+  value.lerp(vector.set(...goal), speed);
+}
