3144c03fbe10b73ce81b3262d8997841a230ead0
Nestor <elnestinatorHD@gmail.com>
2022-10-22 18:25:37 +0200

    Tweak main scene to work with new props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,179 +1,166 @@
 import type { NextPage } from "next";
 import * as THREE from "three";
-import { useLayoutEffect, useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { useLayoutEffect, useRef, useState, useCallback } from "react";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { useTexture, Center, Text3D } from "@react-three/drei";
+import { Bloom, EffectComposer, LUT } from "@react-three/postprocessing";
 import { Reflect } from "../components/reflect";
-import { useTexture } from "@react-three/drei";
+import { LUTCubeLoader } from "postprocessing";
+import { Beam } from "../components/beam";
+import { Rainbow } from "../components/rainbow";
+import { Prism } from "../components/prism";
+import { Flare } from "../components/flare";
+import { Box } from "../components/box";
+import { lerp, lerpV3 } from "../utils/lerp";
+
+export function calculateRefractionAngle(
+  incidentAngle,
+  glassIor = 2.5,
+  airIor = 1.000293
+) {
+  const theta = Math.asin((airIor * Math.sin(incidentAngle)) / glassIor) || 0;
+  return theta;
+}
 
 function Scene() {
-  const streaks = useRef();
-  const glow = useRef();
-  const line = useRef<SVGLineElement | null>(null);
-  const reflect = useRef();
-  const [streakTexture, glowTexture] = useTexture([
-    "https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/1LRW0uiGloWqJcY0WOxREA/61737e55cab34a414d746acb9d0a9400/download.png",
-    "https://assets.vercel.com/image/upload/contentful/image/e5382hct74si/2NKOrPD3iq75po1v0AA6h2/40f1a3d6bc175c89fb0934c8c294254a/download.jpeg",
-  ]);
+  const [isPrismHit, hitPrism] = useState(false);
+  const ambient = useRef<THREE.AmbientLight>(null);
+  const spot = useRef<THREE.SpotLight>(null);
+  const flare = useRef(null);
+  const boxreflect = useRef(null);
+  const rainbow = useRef<THREE.Mesh>(null);
 
-  const obj = new THREE.Object3D();
-  const f = new THREE.Vector3();
-  const t = new THREE.Vector3();
-  const n = new THREE.Vector3();
+  const rayOut = useCallback(() => hitPrism(false), []);
+  const rayOver = useCallback((e) => {
+    // Break raycast so the ray stops when it touches the prism.
+    e.stopPropagation();
+    hitPrism(true);
+    // Set the intensity really high on first contact.
+    rainbow.current.material.speed = 1;
+    rainbow.current.material.emissiveIntensity = 20;
+  }, []);
 
-  let i = 0;
-  let range = 0;
+  const vec = new THREE.Vector3();
+  const rayMove = useCallback(({ api, position, direction, normal }) => {
+    if (!normal) return;
+    // Extend the line to the prisms center.
+    vec.toArray(api.positions, api.number++ * 3);
+    // Set flare.
+    flare.current.position.set(position.x, position.y, -0.5);
+    flare.current.rotation.set(0, 0, -Math.atan2(direction.x, direction.y));
 
-  useLayoutEffect(() => {
-    const geometry = line.current?.geometry;
-    // Bind the positions of the reflector to real line segments.
-    geometry.attributes.position = new THREE.BufferAttribute(
-      reflect.current?.positions,
-      3
+    // Calculate refraction angles.
+    let angleScreenCenter = Math.atan2(-position.y, -position.x);
+    const normalAngle = Math.atan2(normal.y, normal.x);
+
+    // The angle between the ray and the normal.
+    const incidentAngle = angleScreenCenter - normalAngle;
+
+    // Calculate the refraction for the incident angle.
+    const refractionAngle = calculateRefractionAngle(incidentAngle) * 6;
+
+    // Apply the refraction.
+    angleScreenCenter += refractionAngle;
+    rainbow.current.rotation.z = angleScreenCenter;
+
+    // Set spot light.
+    lerpV3(
+      spot.current.target.position,
+      [Math.cos(angleScreenCenter), Math.sin(angleScreenCenter), 0],
+      0.05
     );
-    geometry.attributes.position.usage = THREE.DynamicDrawUsage;
-    geometry.setDrawRange(0, 0);
+    spot.current.target.updateMatrixWorld();
   }, []);
 
   useFrame((state) => {
-    // Bind the ray to your mouse pointer.
-    reflect.current.setRay([
-      (state.pointer.x * state.viewport.width) / 2,
-      (state.pointer.y * state.viewport.height) / 2,
-      0,
-    ]);
-    // Update ray reflection every frame and set draw range to the number of objects that were hit.
-    line.current.geometry.setDrawRange(0, reflect.current.update());
-    line.current.geometry.attributes.position.needsUpdate = true;
+    // Tie beam to the mouse.
+    boxreflect.current.setRay(
+      [
+        (state.pointer.x * state.viewport.width) / 2,
+        (state.pointer.y * state.viewport.height) / 2,
+        0,
+      ],
+      [0, 0, 0]
+    );
 
-    range = reflect.current.update();
+    // Animate rainbow intensity.
+    lerp(
+      rainbow.current.material,
+      "emissiveIntensity",
+      isPrismHit ? 2.5 : 0,
+      0.1
+    );
+    spot.current.intensity = rainbow.current.material.emissiveIntensity;
 
-    for (i = 0; i < range - 1; i++) {
-      // Position 1
-      f.fromArray(reflect.current.positions, i * 3);
-      // Position 2
-      t.fromArray(reflect.current.positions, i * 3 + 3);
-      // Calculate normal
-      n.subVectors(t, f).normalize();
-      // Calculate mid-point
-      obj.position.addVectors(f, t).divideScalar(2);
-      // Stretch by using the distance
-      obj.scale.set(t.distanceTo(f) * 3, 6, 1);
-      // Convert rotation to euler z
-      obj.rotation.set(0, 0, Math.atan2(n.y, n.x));
-      obj.updateMatrix();
-      streaks.current.setMatrixAt(i, obj.matrix);
-    }
-
-    streaks.current.count = range - 1;
-    streaks.current.instanceMatrix.updateRange.count = (range - 1) * 16;
-    streaks.current.instanceMatrix.needsUpdate = true;
-
-    // First glow isn't shown.
-    obj.scale.setScalar(0);
-    obj.updateMatrix();
-    glow.current.setMatrixAt(0, obj.matrix);
-
-    for (i = 1; i < range; i++) {
-      obj.position.fromArray(reflect.current.positions, i * 3);
-      obj.scale.setScalar(0.75);
-      obj.rotation.set(0, 0, 0);
-      obj.updateMatrix();
-      glow.current.setMatrixAt(i, obj.matrix);
-    }
-
-    glow.current.count = range;
-    glow.current.instanceMatrix.updateRange.count = range * 16;
-    glow.current.instanceMatrix.needsUpdate = true;
+    // Animate ambience.
+    lerp(ambient.current, "intensity", 0, 0.025);
   });
 
   return (
     <>
-      <Reflect
-        ref={reflect}
-        far={10}
-        bounce={10}
-        start={[10, 5, 0]}
-        end={[0, 0, 0]}
-      >
-        {/* Any object in here will receive ray events */}
-        <Block scale={0.5} position={[0.25, -0.15, 0]} />
-        <Block scale={0.5} position={[-1.1, 0.9, 0]} rotation={[0, 0, -1]} />
-        <Triangle
-          scale={0.4}
-          position={[-1.1, -1.2, 0]}
-          rotation={[Math.PI / 2, Math.PI, 0]}
+      {/* Lights */}
+      <ambientLight ref={ambient} intensity={0} />
+      <pointLight position={[10, -10, 0]} intensity={0.05} />
+      <pointLight position={[0, 10, 0]} intensity={0.05} />
+      <pointLight position={[-10, 0, 0]} intensity={0.05} />
+      <spotLight
+        ref={spot}
+        intensity={1}
+        distance={7}
+        angle={1}
+        penumbra={1}
+        position={[0, 0, 1]}
+      />
+      {/* Prism + blocks + reflect beam */}
+      <Beam ref={boxreflect} bounce={10} far={20}>
+        <Prism
+          scale={0.6}
+          position={[0, -0.5, 0]}
+          onRayOver={rayOver}
+          onRayOut={rayOut}
+          onRayMove={rayMove}
         />
-      </Reflect>
-      {/* Draw stretched pngs to represent the reflect positions. */}
-      <instancedMesh
-        ref={streaks}
-        args={[null, null, 100]}
-        instanceMatrix-usage={THREE.DynamicDrawUsage}
-      >
-        <planeGeometry />
-        <meshBasicMaterial
-          map={streakTexture}
-          transparent
-          opacity={1}
-          blending={THREE.AdditiveBlending}
-          depthWrite={false}
-          toneMapped={false}
-        />
-      </instancedMesh>
-      {/* Draw glowing dots on the contact points. */}
-      <instancedMesh
-        ref={glow}
-        args={[null, null, 100]}
-        instanceMatrix-usage={THREE.DynamicDrawUsage}
-      >
-        <planeGeometry />
-        <meshBasicMaterial
-          map={glowTexture}
-          transparent
-          opacity={1}
-          blending={THREE.AdditiveBlending}
-          depthWrite={false}
-          toneMapped={false}
-        />
-      </instancedMesh>
+        <Box position={[-1.4, 1, 0]} rotation={[0, 0, Math.PI / 8]} />
+        <Box position={[-2.4, -1, 0]} rotation={[0, 0, Math.PI / -4]} />
+      </Beam>
+      {/* Rainbow and flares */}
+      <Rainbow ref={rainbow} startRadius={0} endRadius={0.5} fade={0} />
+      <Flare
+        ref={flare}
+        visible={isPrismHit}
+        renderOrder={10}
+        scale={1.25}
+        streak={[12.5, 20, 1]}
+      />
     </>
   );
 }
 
-function Block({ onRayOver, ...props }) {
-  const [hovered, hover] = useState(false);
-  return (
-    <mesh
-      onRayOver={(e) => hover(true)}
-      onRayOut={(e) => hover(false)}
-      {...props}
-    >
-      <boxGeometry />
-      <meshBasicMaterial color={hovered ? "orange" : "white"} />
-    </mesh>
-  );
-}
-
-function Triangle({ onRayOver, ...props }) {
-  const [hovered, hover] = useState(false);
-  return (
-    <mesh
-      {...props}
-      onRayOver={(e) => (e.stopPropagation(), hover(true))}
-      onRayOut={(e) => hover(false)}
-      onRayMove={(e) => console.log(e)}
-    >
-      <cylinderGeometry args={[1, 1, 1, 3, 1]} />
-      <meshBasicMaterial color={hovered ? "hotpink" : "white"} />
-    </mesh>
-  );
-}
-
 const Home: NextPage = () => {
+  const texture = useLoader(
+    LUTCubeLoader,
+    "https://uploads.codesandbox.io/uploads/user/b3e56831-8b98-4fee-b941-0e27f39883ab/DwlG-F-6800-STD.cube"
+  );
+
   return (
-    <Canvas orthographic camera={{ zoom: 100 }} style={{ height: "99vh" }}>
-      <color attach="background" args={["#000"]} />
+    <Canvas
+      orthographic
+      gl={{ antialias: false }}
+      camera={{ position: [0, 0, 100], zoom: 70 }}
+      style={{ height: "99vh" }}
+    >
+      <color attach="background" args={["black"]} />
       <Scene />
+      <EffectComposer disableNormalPass>
+        <Bloom
+          mipmapBlur
+          levels={9}
+          intensity={1.5}
+          luminanceThreshold={1}
+          luminanceSmoothing={1}
+        />
+        <LUT lut={texture} />
+      </EffectComposer>
     </Canvas>
   );
 };
